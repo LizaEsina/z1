f2ddf87705f82e5a63236545427a4916881ab26d
Есина Елизавета Дмитриевна <1157-21@std.pkgh.edu>
2023-09-16 14:48:56 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,25 @@
+fun main() {
+    println("Введите строку:")
+    val input = readLine()
+    var count = 0
+    var prevChar: Char? = null
+    var result = ""
+
+    input?.forEach { char -> if (char != prevChar) {
+        if (count >= 2) {
+            result += count.toString()
+        }
+        result += char
+        count = 1
+        prevChar = char
+    } else {
+        count++
+    }
+    }
+
+    if (count >= 2) {
+        result += count.toString()
+    }
+
+    println("Результат: $result")
+}
